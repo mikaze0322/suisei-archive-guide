6a8164e1cdc2453b15c769bbc0c51156913dba57
aozaki-kuro <aozaki.kuro@gmail.com>
2021-09-17 10:17:47 +0900

    ignore

--- docs/.vuepress/.temp/pages/index.html.vue
+++ docs/.vuepress/.temp/pages/index.html.vue
@@ -1,8 +1,7 @@
 <template><h1 id="introduction" tabindex="-1"><a class="header-anchor" href="#introduction" aria-hidden="true">#</a> Introduction</h1>
-<p style="text-align:center; font-style:italic;"><img src="/android-chrome-192x192.png" width="100rem"><br>Treasure every moment of our dear comet</p>
+<p style="text-align:center; font-style:italic; font"><img src="/android-chrome-192x192.png" width="100rem"><br>Treasure every moment of our dear comet</p>
 <h2 id="what-is-this" tabindex="-1"><a class="header-anchor" href="#what-is-this" aria-hidden="true">#</a> What is this?</h2>
 <p>This is a brief archiving guide written for Hoshiyomis to archive every stream of Suisei.</p>
-<p>Purpose: <em>Treasure every moment of our dear comet.</em></p>
 <h2 id="important" tabindex="-1"><a class="header-anchor" href="#important" aria-hidden="true">#</a> IMPORTANT</h2>
 <p>IF YOU HAVE ANY PROBLEMS, READ THE <code>README.MD</code> IN EVERY REPO FIRST.</p>
 <h2 id="changelog" tabindex="-1"><a class="header-anchor" href="#changelog" aria-hidden="true">#</a> Changelog</h2>
@@ -26,7 +25,7 @@
 <li>This Guide was written under the <strong>CC BY-NC-SA 4.0</strong> license, please confirm the guidelines before reposting.</li>
 <li>This Guide does not provide any tool or script to grab DRM protected content.</li>
 <li>This Guide is made for Hoshiyomis to archive Suisei's activities on YouTube, Niconico and other streaming services.</li>
-<li>The copyright of all contents belongs to Hoshimachi Suisei / Hololive / Cover Corp. and the proper copyright holder/s.</li>
+<li>The copyright of all contents archived belongs to Hoshimachi Suisei / Hololive / Cover Corp. and the proper copyright holder/s.</li>
 <li>This Guide will be deleted if there is a formal takedown request.</li>
 <li>Please do not redistribute / re-upload the content archived on public channels. This applies especially to distribution for Baidu Tieba / Bilibili / NGA / QQ groups.</li>
 <li>If you need materials to make a clip or introduction of Suisei, please be sure it's positive and won't damage her reputation.</li>
@@ -36,5 +35,5 @@
 <h2 id="credit" tabindex="-1"><a class="header-anchor" href="#credit" aria-hidden="true">#</a> Credit</h2>
 <p>Site created using <a href="https://vuepress.vuejs.org/" target="_blank" rel="noopener noreferrer">VuePress<OutboundLink/></a></p>
 <p>Deployed on <a href="http://vercel.com/" target="_blank" rel="noopener noreferrer">Vercel<OutboundLink/></a></p>
-<p>Search (is going to be) provided by <a href="https://docsearch.algolia.com/" target="_blank" rel="noopener noreferrer">DocSearch<OutboundLink/></a></p>
+<p>Search (will be) provided by <a href="https://docsearch.algolia.com/" target="_blank" rel="noopener noreferrer">DocSearch<OutboundLink/></a></p>
 </template>
